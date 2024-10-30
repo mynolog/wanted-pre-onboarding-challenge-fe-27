461963c4d96cdd3613e4b1a8b5682208a4ac3e7b
mynolog <minholee.log@gmail.com>
2024-10-30 22:48:15 +0900

    feat: 회원가입 UI, 서비스 로직 구현

---
+++ src/services/api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost'
+const PORT = '8080'
+
+const axiosInstance = axios.create({
+  baseURL: `${BASE_URL}:${PORT}`,
+  timeout: 2000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export default axiosInstance
