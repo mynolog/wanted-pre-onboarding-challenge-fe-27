3453413fdfc737388c306c85738c31cfc9b3ea1b
mynolog <minholee.log@gmail.com>
2024-10-30 19:45:38 +0900

    feat: 라우트 기본 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
-}
+import { Outlet } from 'react-router-dom'
+import Router from './routes'
 
+const App = () => {
+  return (
+    <>
+      <Router />
+      <Outlet />
+    </>
+  )
+}
 export default App
---
+++ src/routes/route.tsx
@@ -0,0 +1,15 @@
+const route = [
+  {
+    path: '/',
+    element: <div>home</div>,
+  },
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+  {
+    path: '/create',
+    element: <div>create</div>,
+  },
+]
+export default route
