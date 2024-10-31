48232f98eed7a3781cba5f69d2cd157d1427d2c4
mynolog <minholee.log@gmail.com>
2024-10-31 22:43:40 +0900

    feat: 로그인, 회원가입 입력값 검증 로직 추가

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,7 +1,8 @@
-import type { MouseEvent } from 'react'
-import { useState } from 'react'
+import { ChangeEvent, MouseEvent } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
+import { useValidate } from '../../hooks/useValidate'
 import CommonInput from '../../components/common/input/CommonInput'
 import CommonButton from '../../components/common/button/CommonButton'
 import FormTitle from '../../components/common/title/FormTitle'
@@ -11,6 +12,12 @@ import Logo from '../../components/common/logo/Logo'
 
 const SignUp = () => {
   const [responseError, setResponseError] = useState('')
+  const {
+    validErrors,
+    validateEmail,
+    validatePassword,
+    validatePasswordConfirm,
+  } = useValidate()
   const { form, handleFormChange, resetForm } = useForm(
     {
       email: '',
@@ -21,9 +28,30 @@ const SignUp = () => {
   )
   const navigate = useNavigate()
 
+  useEffect(() => {
+    console.log(validErrors)
+  }, [validErrors])
+
+  const isButtonDisabled =
+    !form.email ||
+    !form.password ||
+    !form.passwordConfirm ||
+    !!validErrors.email ||
+    !!validErrors.password ||
+    !!validErrors.passwordConfirm
+
   const handleSignUpSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     const { email, password } = form
+
+    if (
+      validErrors.email ||
+      validErrors.password ||
+      validErrors.passwordConfirm
+    ) {
+      return
+    }
+
     const signUpForm = {
       email,
       password,
@@ -43,33 +71,53 @@ const SignUp = () => {
     }
   }
 
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleFormChange(e)
+    validateEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleFormChange(e)
+    validatePassword(e.target.value)
+  }
+
+  const handlePasswordConfirmChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleFormChange(e)
+    validatePasswordConfirm(form.password, e.target.value)
+  }
+
   return (
     <Background>
       <div className="w-full h-svh flex justify-center items-center">
-        <form className="w-6/12 h-4/5 flex flex-col items-center justify-center gap-3 rounded-2xl p-8 shadow-2xl bg-white z-50">
+        <form className="w-6/12 h-4/5 flex flex-col items-center justify-center gap-2 rounded-2xl p-8 shadow-2xl bg-white z-50">
           <Logo top="0" width="235px" height="235px" />
           <FormTitle title="회원가입" />
           <CommonInput
             name="email"
             label="이메일"
             value={form.email}
-            onChange={handleFormChange}
+            onChange={handleEmailChange}
           />
           <CommonInput
             name="password"
             label="비밀번호"
             type="password"
             value={form.password}
-            onChange={handleFormChange}
+            onChange={handlePasswordChange}
           />
           <CommonInput
             name="passwordConfirm"
             label="비밀번호 확인"
             type="password"
             value={form.passwordConfirm}
-            onChange={handleFormChange}
+            onChange={handlePasswordConfirmChange}
           />
-          <CommonButton type="submit" width="65%" onClick={handleSignUpSubmit}>
+          <CommonButton
+            type="submit"
+            width="65%"
+            onClick={handleSignUpSubmit}
+            disabled={isButtonDisabled}
+          >
             가입하기
           </CommonButton>
           {responseError && (
